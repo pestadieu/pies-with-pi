e46b99c6c0549f191d84f5d1fc89c12cc99e4abe
Zachary Feng <zachary.feng@hotmail.com>
2018-05-26 23:08:41 +0000

    New QR code reader

--- camera.py
+++ camera.py
@@ -2,36 +2,51 @@ import os
 import time
 from threading import Thread
 from queue import Queue
-import qrtools
 from picamera import PiCamera
+
+camera = PiCamera()
 
 class Camera(Thread):
 
 	def __init__(self, q_write):
 		Thread.__init__(self)
 		self.name = "camera_thread"
-		self.camera = PiCamera()
 		self.q_write = q_write
 
 	def run(self):
 		while True:
 			time.sleep(1)
-			self.take_photo()
-			# Process your photo, do everything you want here
-			qr = qrtools.QR()
-			qr.decode('pic.jpg')
-			if qr.data is None:
-				cooking_time = 0
-			else:
-				cooking_time = int(qr.data) # in seconds (otherwise decimals)
+                        self.take_photo()
+                        # Process your photo, do everything you want here
+                        os.system("zbarimg " + "pic.jpg > code.txt")
+                        with open('code.txt', "r") as text_file:
+                            data = text_file.readline()
+                            
+                        if not data:
+                            print("not QR code")
+                            cooking_time = 0
+                        else:
+                            data = data.split(':')
+                            cooking_time = int(data[1])
+                            print(cooking_time)
+
 			#According to the image, put the required cooking time here. If no cooking is required, put 0
 			if cooking_time != 0:
 				self.q_write.put(str(cooking_time))
 	
 	def take_photo(self):
-		self.camera.capture('pic.jpg')
+		camera.capture('pic.jpg')
 
 if __name__ == "__main__":
-    c = Picamera()
-    c.capture('pic.jpg)
+    camera.capture('pic.jpg')
     
+    os.system("zbarimg " + "pic.jpg > code.txt")
+    
+    with open('code.txt', "r") as text_file:
+        data = text_file.readline()
+
+    if not data:
+        print("not QR code")
+    else:
+        data = data.split(':')
+        print(int(data[1]))
