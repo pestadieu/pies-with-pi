ba5beba4515a8481e23188558ad91af47a8e01e2
pestadieu <estadieu.pl@gmail.com>
2018-05-26 18:10:16 +0200

    Turn spaces to tab

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 import os
 import time
 from threading import Thread
-from Queue import Queue
+from queue import Queue
 import qrtools
 
 def take_photo():
@@ -18,16 +18,13 @@ class Camera(Thread):
 		while True:
 			sleep(1)
 			take_photo()
-						# Process your photo, do everything you want here
-
-						qr = qrtools.QR()
-						qr.decode('pic.jpg')
-
-						if qr.data is None:
-							cooking_time = 0
-						else:
-							cooking_time = int(qr.data) # in seconds (otherwise decimals)
-
-						#According to the image, put the required cooking time here. If no cooking is required, put 0
+			# Process your photo, do everything you want here
+			qr = qrtools.QR()
+			qr.decode('pic.jpg')
+			if qr.data is None:
+				cooking_time = 0
+			else:
+				cooking_time = int(qr.data) # in seconds (otherwise decimals)
+			#According to the image, put the required cooking time here. If no cooking is required, put 0
 			if cooking_time != 0:
 				q_write.put(str(cooking_time))
