16aceffc0e33c00f4ef7dea020a1eaffa1b5e10c
Zachary Feng <zachary.feng@hotmail.com>
2018-05-26 16:15:27 +0000

    Solve minors bugs

--- main.py
+++ main.py
@@ -3,12 +3,13 @@
 from queue import Queue
 from chatbot import *
 from camera import *
+from door_state import *
 
 if(__name__ == '__main__'):
 	queue_camera_chatbot = Queue() # queue from the camera to the chatbot
 
 	thread_cam = Camera(queue_camera_chatbot)   # Camera thread
-	thread_chat = Chabot(queue_camera_chatbot)  # Chatbot thread
+	thread_chat = Chatbot(queue_camera_chatbot)  # Chatbot thread
 	thread_door = DoorState()  # Door state thread
 	
 	thread_cam.start()
