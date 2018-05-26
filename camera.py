528c2daa70f212197c64fd87a74a96c9636e3e4b
pestadieu <estadieu.pl@gmail.com>
2018-05-26 17:07:23 +0200

    Convert space to tabs

--- camera.py
+++ camera.py
@@ -6,29 +6,28 @@ import qrtools
 
 def take_photo():
 	camera.capture('pic.jpg')
-	
+
 class Camera(Thread):
-	
+
 	def __init__(self, q_read):
 		Thread.__init__(self)
 		self.name = "camera_thread"
 		self.q_write = q_write
-	
+
 	def run():
 		while True:
 			sleep(1)
 			take_photo()
-                        # Process your photo, do everything you want here
-                        
-                        qr = qrtools.QR()
-                        qr.decode('pic.jpg')
-                        
-                        if qr.data is None:
-                            cooking_time = 0
-                        else:
-                            cooking_time = int(qr.data) # in seconds (otherwise decimals)
+						# Process your photo, do everything you want here
 
-                        #According to the image, put the required cooking time here. If no cooking is required, put 0
+						qr = qrtools.QR()
+						qr.decode('pic.jpg')
+
+						if qr.data is None:
+							cooking_time = 0
+						else:
+							cooking_time = int(qr.data) # in seconds (otherwise decimals)
+
+						#According to the image, put the required cooking time here. If no cooking is required, put 0
 			if cooking_time != 0:
 				q_write.put(str(cooking_time))
-	
