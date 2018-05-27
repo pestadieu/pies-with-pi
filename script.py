df40d92edd145aeb17f360d7ab3c2b9f26fac31d
pestadieu <estadieu.pl@gmail.com>
2018-05-27 10:15:56 +0200

    Add features to the camera

--- script.py
+++ script.py
@@ -1,5 +1,10 @@
 import time
+import os
 import handle_lcd as hl
+from camera import *
+from keras.models import load_model, Sequential
+from PIL import Image
+import numpy as np
 
 def init():
     GPIO.setmode(GPIO.BCM)
@@ -21,7 +26,31 @@ def timer(timeout):
     return
 
 def camera_read():
-    return 3
+    camera.capture("pic.jpg")
+    os.system("zbarimg " + "pic.jpg > code.txt")
+
+    with open('code.txt', "r") as text_file:
+        data = text_file.readline()
+
+    if not data:
+        print("No QR code: Computer vision")
+        print("The object is")
+        img = Image.open()
+        img.resize((100, 100), Image.ANTIALIAS)
+        img = np.array(img).astype(np.uint8)
+        img = np.expand_dims(img, axis=0)
+        model = load_model("fruit_model.h5")
+        print(model.predict_classes(img))
+        cooking_time = 0
+    else:
+        data = data.split(":")
+        cooking_time = int(data[1])
+        print(cooking_time)
+
+    if cooking_time == 0:
+        return model.predict_classes(img)
+    else:
+        return cooking_time
 
 if __name__ == "__main__":
     thread_chat = Chatbot()  # Chatbot thread
