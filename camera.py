d4a90be25a8253ebc452328817fbbaf829fdb17d
Zachary Feng <zachary.feng@hotmail.com>
2018-05-27 07:38:24 +0000

    computer vision

--- camera.py
+++ camera.py
@@ -3,50 +3,59 @@ import time
 from threading import Thread
 from queue import Queue
 from picamera import PiCamera
+from PIL import Image
+import numpy as np
+from keras.models import load_model, Sequential
 
 camera = PiCamera()
 
 class Camera(Thread):
+    def __init__(self, q_write):
+        Thread.__init__(self)
+        self.name = "camera_thread"
+        self.q_write = q_write
 
-	def __init__(self, q_write):
-		Thread.__init__(self)
-		self.name = "camera_thread"
-		self.q_write = q_write
+    def run(self):
+        while True:
+            time.sleep(1)
+            self.take_photo()
+            # Process your photo, do everything you want here
+            os.system("zbarimg " + "pic.jpg > code.txt")
+            with open('code.txt', "r") as text_file:
+                data = text_file.readline()
+            
+            if not data:
+                print("not QR code")
+                print("the fruit is...")
+                img = Image.open("pic.jpg")
+                img = img.resize((100,100), Image.ANTIALIAS)
+                img = np.array(img).astype(np.uint8)
+                img = np.expand_dims(img,axis=0)
+                model = load_model("fruit_model.h5")
+                print(model.predict_classes(img))
+                cooking_time = 0
+            else:
+                data = data.split(':')
+                cooking_time = int(data[1])
+                print(cooking_time)
 
-	def run(self):
-		while True:
-			time.sleep(1)
-			self.take_photo()
-			# Process your photo, do everything you want here
-			os.system("zbarimg " + "pic.jpg > code.txt")
-			with open('code.txt', "r") as text_file:
-				data = text_file.readline()
+	        #According to the image, put the required cooking time here. If no cooking is required, put 0
+            if cooking_time != 0:
+                self.q_write.put(str(cooking_time))
 
-			if not data:
-				print("not QR code")
-				cooking_time = 0
-			else:
-				data = data.split(':')
-				cooking_time = int(data[1])
-				print(cooking_time)
-
-			#According to the image, put the required cooking time here. If no cooking is required, put 0
-			if cooking_time != 0:
-				self.q_write.put(str(cooking_time))
-
-	def take_photo(self):
-		camera.capture('pic.jpg')
+    def take_photo(self):
+	    camera.capture('pic.jpg')
 
 if __name__ == "__main__":
-	camera.capture('pic.jpg')
+    camera.capture('pic.jpg')
 
-	os.system("zbarimg " + "pic.jpg > code.txt")
+    os.system("zbarimg " + "pic.jpg > code.txt")
 
-	with open('code.txt', "r") as text_file:
-		data = text_file.readline()
+    with open('code.txt', "r") as text_file:
+        data = text_file.readline()
 
-	if not data:
-		print("not QR code")
-	else:
-		data = data.split(':')
-		print(int(data[1]))
+    if not data:
+        print("not QR code")
+    else:
+        data = data.split(':')
+        print(int(data[1]))
