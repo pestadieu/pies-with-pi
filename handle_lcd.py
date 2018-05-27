b7cf2f1f4e9d83c60f0b384a95e47a2cd0251af4
Zachary Feng <zachary.feng@hotmail.com>
2018-05-27 00:47:01 +0000

    Nothings works

--- handle_lcd.py
+++ handle_lcd.py
@@ -8,6 +8,7 @@ lcd = CharLCD(cols=16, rows=2, pin_rs=4, pin_e=17, pins_data=[18, 22, 23, 24], n
     # ~ lcd.write_string("Time: %s" %time.strftime("%H:%M:%S"))
     
 def printTime(inputTime):
+    clear()
     lcd.write_string(str(inputTime) + " secondes")
     return
 
